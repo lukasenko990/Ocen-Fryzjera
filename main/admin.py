44099348b3e405030e02073e6818569757c8aef1
lukasenko990 <lukasenko990@gmail.com>
2021-04-20 12:16:12 +0200

    Add project files

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Salon, Usluga, Fryzjer, Klient, Zamowienie
-
-
-admin.site.register(Salon)
-admin.site.register(Usluga)
-admin.site.register(Fryzjer)
-admin.site.register(Klient)
-admin.site.register(Zamowienie)
