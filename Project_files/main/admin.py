c25e757888d06e4888a47c958503a66245daa6f7
lukasenko990 <lukasenko990@gmail.com>
2021-04-20 16:19:29 +0200

    Add project files

---
+++ Project_files/main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Salon, Usluga, Fryzjer, Klient, Zamowienie
+
+
+admin.site.register(Salon)
+admin.site.register(Usluga)
+admin.site.register(Fryzjer)
+admin.site.register(Klient)
+admin.site.register(Zamowienie)
---
+++ Project_files/main/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+class FryzjerUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
